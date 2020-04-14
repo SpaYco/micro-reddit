19ab92fd193c99b3f17a1acf39ed8941ff111b53
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-14 15:36:31 +0500

    test validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 20}
+    validates :email, presence: true, uniqueness: { case_sensitive: false },
+    format: { with: ConstantData::VALID_EMAIL_REGEX }
+    
+    
+    
+    
 end
--- db/migrate/20200414100122_create_users.rb
+++ db/migrate/20200414100122_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :name
+      t.integer :age
+      t.string :email
+      t.integer :karma
+      t.string :password
 
       t.timestamps
     end
