0bdd4ff4f3c8bbfa868e4878d2c172bedf2cadf1
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-16 15:24:23 +0500

    user comment relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 20 }  
   validates :email, presence: true, uniqueness: { case_sensitive: false },  format: { with: URI::MailTo::EMAIL_REGEXP } 
   has_many :posts
+  has_many :comments
 end
